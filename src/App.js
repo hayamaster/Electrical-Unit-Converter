46bbd28e0f1423fad32a8f9947ca4d66780a4c51
kimjuha <dnfxmfkdbzos123@gmail.com>
2023-01-03 02:30:49 +0900

    create title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import {useState, useEffect} from 'react';
+import styles from './App.module.css';
 
 function App() {
   return (
     <div>
-      <h1>sival</h1>
+      <h1 className={styles.title}>Converter of Electrical Units</h1>
+      <div className={styles.container_logo}>
+        <h3 className={styles.h3}>Gachon Univ. Electrical Engineering</h3>
+        <img className={styles.logo} alt='electricity_logo' src='flash.png'/>
+        <h4 className={styles.h4}>Created by. JUHA</h4>
+      </div>
     </div>
   );
 }
