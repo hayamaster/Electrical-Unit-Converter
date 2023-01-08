4fa0726e3d327b3671845839e18ba009bcc7a8aa
hayamaster <dnfxmfkdbzos@naver.com>
2023-01-08 17:49:37 +0900

    modify logo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <h1 className={styles.title}>Converter of Electrical Units</h1>
       <div className={styles.container_logo}>
         <h3 className={styles.h3}>Gachon Univ. Electrical Engineering</h3>
-        <img className={styles.logo} alt='electricity_logo' src='flash.png'/>
+        <img className={styles.logo} alt='electricity_logo' src={process.env.PUBLIC_URL + '/flash.png'}/>
         <h4 className={styles.h4}>Created by. JUHA</h4>
       </div>
       <Converter />
